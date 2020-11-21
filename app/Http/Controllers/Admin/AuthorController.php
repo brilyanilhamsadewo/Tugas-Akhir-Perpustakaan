79e6e9958e1472c0d611ccf9fce0fef0f6ec702d
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-11-21 17:29:30 +0700

    Menambahkan Flash Message

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -38,7 +38,7 @@ class AuthorController extends Controller
     {
         Author::create($request->only('name'));
 
-        return redirect()->route('admin.author.index');
+        return redirect()->route('admin.author.index')->with('success','Data penulis berhasil ditambahkan');
     }
 
     /**
@@ -76,7 +76,7 @@ class AuthorController extends Controller
     {
         $author->update($request->only('name'));
 
-        return redirect()->route('admin.author.index');
+        return redirect()->route('admin.author.index')->with('info','Data penulis berhasil diupdate');
     }
 
     /**
@@ -89,6 +89,6 @@ class AuthorController extends Controller
     {
         $author->delete();
 
-        return redirect()->route('admin.author.index');
+        return redirect()->route('admin.author.index')->with('danger','Data penulis berhasil terhapus');
     }
 }
--- resources/views/admin/author/index.blade.php
+++ resources/views/admin/author/index.blade.php
@@ -8,6 +8,9 @@
         </div>
         
         <div class="box-body">
+
+            @include('admin.templates.partials.alerts')
+
             <table id="dataTable" class="table table-bordered table-hover">
                 <thead>
                     <tr>
