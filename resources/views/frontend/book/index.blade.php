d59e3ca23ccd6c41276607169f6a5d5dfa809c59
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-11-23 11:59:22 +0700

    Membuat Pagination Custom

--- resources/views/frontend/book/index.blade.php
+++ resources/views/frontend/book/index.blade.php
@@ -28,14 +28,6 @@
 
     {{-- pagination --}}
 
-    {{ $books->links() }}
-    {{-- <ul class="pagination center">
-        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-        <li class="active"><a href="#!">1</a></li>
-        <li class="waves-effect"><a href="#!">2</a></li>
-        <li class="waves-effect"><a href="#!">3</a></li>
-        <li class="waves-effect"><a href="#!">4</a></li>
-        <li class="waves-effect"><a href="#!">5</a></li>
-        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-    </ul> --}}
+    {{ $books->links('vendor.pagination.materialize') }}
+    
 @endsection
